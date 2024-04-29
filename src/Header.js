cf95cb3d9c6caae6e645237f5b8b5d19063b5cf6
brandaorichard <rih.brandao@gmail.com>
2024-04-29 04:46:42 -0300

    feat: Header

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header>
+      <Link to="/" className="logo">
+        My Blog
+      </Link>
+      <nav>
+        <Link to="/login" className="logo">
+          Login
+        </Link>
+        <Link to="/register" className="logo">
+          Register
+        </Link>
+      </nav>
+    </header>
+  );
+}
