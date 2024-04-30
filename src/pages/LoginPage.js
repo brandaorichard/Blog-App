269cd14367332a716ff630a898f99b5f37ebde5f
brandaorichard <rih.brandao@gmail.com>
2024-04-29 21:17:06 -0300

    feat: login & register page

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -2,21 +2,11 @@ import React from 'react';
 
 export default function LoginPage() {
   return (
-    <div className="login-container">
-      <div className="login-form">
-        <h2>Login</h2>
-        <form>
-          <div className="input-group">
-            <label htmlFor="username">Username:</label>
-            <input type="text" id="username" name="username" />
-          </div>
-          <div className="input-group">
-            <label htmlFor="password">Password:</label>
-            <input type="password" id="password" name="password" />
-          </div>
-          <button type="submit">Login</button>
-        </form>
-      </div>
-    </div>
+    <form className="login">
+      <h1>Login</h1>
+      <input type="text" placeholder="username"></input>
+      <input type="password" placeholder="password"></input>
+      <button>Login</button>
+    </form>
   );
 }
